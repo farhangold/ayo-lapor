8cdd47ef85541b483228eaaeeff8ff4d9602d602
farhangold <121116261+farhangold@users.noreply.github.com>
2023-10-31 15:06:41 +0700

    Login

--- application/views/modul-login/login.php
+++ application/views/modul-login/login.php
@@ -12,49 +12,51 @@
         }
     </style>
 
-
+    
 </head>
 <body>
     <div>
         <div class="row">
-            <div class="col-md-6 justify-content-center" >
+            <div class="col-md-7 justify-content-center" >
             <div style="padding:150px;">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login to AyoLapor</h1><br>
-                                        <h2 class="h4 text-gray-700 mb-1">Welcome to AyoLapor! Let's Report and Build a Better Future</h2>
-                                    </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <label for="userType"></label>
-                                            <select select class="form-control" id="userType">
-                                                <option value="mop">Pilih Role Anda!</option>
-                                                <option value="user">Mahasiswa</option>
-                                                <option value="admin">Dosen</option>
-                                                <option value="guest">Kemahasiswaan</option>
-                                            </select>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukkan Email SSO Anda">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                                <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                            </div>
-                                        </div>
-                                        <a type="submit" class="btn btn-danger btn-user btn-block">
-                                            Login
-                                        </a>
-
-                                    </form>
-                                </div>
-            </div>
-            <div class="col-md-6 loginbg">
+                 <div class="text-center">
+                     <h1>Login to AyoLapor</h1><br>
+                     <h2>Welcome to AyoLapor! Let's Report and Build a Better Future</h2><br>
+                 </div>
+                 <form class="user">
+                     <div class="form-group row">
+                         <label for="userType"></label>
+                         <select select class="form-control" id="userType">
+                             <option value="mop">Pilih Role Anda!</option>
+                             <option value="user">Mahasiswa</option>
+                             <option value="admin">Dosen</option>
+                             <option value="guest">Kemahasiswaan</option>
+                         </select>
+                     </div>
+                     <div class="form-group row">
+                         <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Masukkan Email SSO Anda">
+                     </div>
+                     <div class="form-group row">
+                         <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                     </div>
+                     <div class="form-group row">
+                        <div class="col-6 custom-control custom-checkbox small text-left">
+                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                            <label class="custom-control-label" for="customCheck">Remember Me</label>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a href="forgot-password.html">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <a type="submit" class="btn btn-danger btn-user btn-block">
+                            Login
+                        </a>
+                     </div>
+                 </form>
+                </div>
+                </div>
+            <div class="col-md-5 loginbg">
             </div>
         </div>
     </div>

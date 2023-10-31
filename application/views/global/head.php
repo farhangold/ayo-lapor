35373172bb0c807650676f596856e8138e1ff27b
Ghazaaaaa <gymnastiarghaza@gmail.com>
2023-10-31 22:48:38 +0700

    dasboard

--- application/views/global/head.php
+++ application/views/global/head.php
@@ -14,35 +14,66 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>" />
     <style>
-        .navbar {
-            z-index: 1000; 
-        }
-        .content {
-            padding-top: 70px;
-        }
-        .square {
-            width: 1080px; /* Lebar garis persegi */
-            height: 500px; /* Tinggi garis persegi */
-            border: 2px solid red; /* Warna dan ketebalan garis tepi */
-            display: flex;
-            justify-content: center; /* Tengah horizontal */
-            align-items: center; /* Tengah vertikal */
-        }
-        .squareimage {
-        width: 1080px;
-        height: 500px;
+      .navbar {
+          z-index: 1000; 
+      }
+      
+      .content {
+          padding-top: 70px;
+      }
+      .square {
+        max-width: 100%; /* Ensure the square doesn't exceed the container width */
+        height: auto; /* Allow the height to adjust proportionally */
+        border: 2px solid red; /* Color and border thickness */
         display: flex;
-        justify-content: center;
-        align-items: center;
+        justify-content: center; /* Horizontal center */
+        align-items: center; /* Vertical center */
+      }
+      .squareimage {
+        max-width: 100%; /* Ensure the image doesn't exceed the container width */
+        height: auto; /* Allow the height to adjust proportionally */
+        display: flex;
+        justify-content: center; /* Horizontal center */
+        align-items: center; /* Vertical center */
         background-image: url('<?= base_url("assets") ?>/images/pana.png');
-        background-size: auto 90%; /* Set the width to auto, and height to 100% */
+        background-size: cover; /* Cover the entire container */
         background-repeat: no-repeat; /* Prevent image from repeating */
-        background-position: center; 
+        background-position: center;
+      }
+      @media (max-width: 768px) {
+        .square, .squareimage {
+        max-width: 100%;
+        height: auto;
         }
-
-        .text {
-            color: black; /* Warna teks */
+      }
+      .text {
+        color: black; /* Warna teks */
+      }
+      .carousel-inner {
+        overflow: hidden;
+      }
+      .carousel-item .col-md-4 {
+        margin-right: 10px; /* Adjust this value to control spacing */
+      }
+      .carousel-inner .carousel-item {
+        text-align: center;
+      }
+      .square {
+        background-color: #f2f2f2;
+        padding: 20px;
+      }
+      .square p {
+        font-size: 18px;
+      }
+      @media (max-width: 768px) {
+        .carousel-inner .carousel-item .col-md-4 {
+          margin-right: 0;
+          margin-bottom: 10px; /* Adjust this value for spacing between items on smaller screens */
         }
+        .carousel-inner .carousel-item .col-md-4:last-child {
+          margin-bottom: 0;
+        }
+      }
     </style>
   </head>
-  <body>
+<body>

276704d85826044df0c1d16b3104001655927e9f
Bengley123 <yesasihombing1@gmail.com>
2024-01-08 21:59:30 +0700

    Melengkapi Profil views

--- application/views/modul-profile/profile_Dosen.php
+++ application/views/modul-profile/profile_Dosen.php
@@ -1,11 +1,15 @@
 <div class="mycontainer" style="margin-top: 1rem;">
     <div class="row">
         <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
             <!-- Tambahkan gambar profil di sini -->
             <img src="<?= base_url('assets') ?>/images/user.png" alt="Profile Image" class="img-fluid">
             <p><strong></strong> <?= $profile['first_name']." ".$profile['last_name'] ?></p>
             <p><strong></strong> <?= $profile['nip'] ?></p>
             <p><strong></strong> <?= $profile['role'] ?></p>
+                </div>
+            </div>
         </div>
         <div class="col-md-9">
             <div class="card">
--- application/views/modul-profile/profile_Kmh.php
+++ application/views/modul-profile/profile_Kmh.php
@@ -2,20 +2,26 @@
     <div class="row">
         <div class="col-md-3">
             <div class="card">
+                <div class="card-body">
                 <!-- Tambahkan gambar profil di sini -->
                 <img src="<?= base_url('assets') ?>/images/user.png" alt="Profile Image" class="img-fluid">
                 <p><strong></strong> <?= $profile['first_name']." ".$profile['last_name'] ?></p>
                 <p><strong></strong> <?= $profile['role'] ?></p>
+                </div>
             </div>
         </div>
         <div class="col-md-9">
-            <h1>Profile Kemahasiswaan</h1><hr>
-            <p><strong>First Name:</strong> <?php echo $profile['first_name']; ?></p>
-            <p><strong>Last Name:</strong> <?php echo $profile['last_name']; ?></p>
-            <p><strong>Email Address:</strong> <?php echo $profile['email']; ?></p>
-            <p><strong>Profile Description:</strong> <?php echo $profile['description']; ?></p>
+            <div class="card">
+                <div class="card-body">
+                    <h1>Profile Kemahasiswaan</h1><hr>
+                    <p><strong>First Name:</strong> <?php echo $profile['first_name']; ?></p>
+                    <p><strong>Last Name:</strong> <?php echo $profile['last_name']; ?></p>
+                    <p><strong>Email Address:</strong> <?php echo $profile['email']; ?></p>
+                    <p><strong>Profile Description:</strong> <?php echo $profile['description']; ?></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-<br><br><br><br>
+<br><br><br><br><br>
   
